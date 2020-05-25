d4184377122274723c2e2da2df7f30abb08ede02
yannick <yannick.schepens@vdabcampus.be>
2020-05-25 23:36:49 +0200

    Add project files.

---
+++ GitCursus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
