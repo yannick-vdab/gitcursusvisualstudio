47ca473687e32ecf3bb4871503ce04fbd3223d62
yannick <yannick.schepens@vdabcampus.be>
2020-05-25 23:39:31 +0200

    eerste commit

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -6,7 +6,7 @@ namespace GitCursus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo");
         }
     }
 }
